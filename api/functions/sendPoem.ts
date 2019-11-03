72ff6533274e19bf7d4da35f1ba82c71cff1d12c
Peter Smith <peter@upstateinteractive.io>
2019-11-02 22:31:05 -0400

    feat(api): send title

--- api/functions/sendPoem.ts
+++ api/functions/sendPoem.ts
@@ -3,7 +3,8 @@ import { sendMessage, delay } from './'
 
 
 export async function sendPoem (poem: Poem, sender: number): Promise<boolean> {
-  const { lines } = poem
+  const { lines, title } = poem
+  await sendMessage(title, sender)
   while (lines.length) {
     const line = lines[0]
     await sendMessage(line.text, sender)
