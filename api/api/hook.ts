09cd056e94ed99d0ecc23f07ccb1891df4aee98c
Peter Smith <peter@upstate.agency>
2019-12-29 17:30:03 -0500

    refactor: do not parse text

--- api/api/hook.ts
+++ api/api/hook.ts
@@ -1,15 +1,12 @@
 import { NowRequest, NowResponse } from '@now/node'
 import {
   getSenderNextPoem,
-  sendPoem,
-  parseText
+  sendPoem
 } from '../functions'
 
 export default async (req: NowRequest, res: NowResponse) => {
   const text = req.body.message.content.text
   const fbRecipientId = +req.body.from.id
-
-  parseText(text)
   const poem = await getSenderNextPoem(fbRecipientId)
   await sendPoem(poem, fbRecipientId)
   res.status(200).send(null)
